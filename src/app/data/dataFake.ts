04164c611debb4823ef7cdd77e1f0d26f2ca0de8
All <alissonantonio69@gmail.com>
2023-10-06 00:11:37 -0300

    projeto feito

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,32 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "3 Metódos Assertivos de Leitura de Cartas!",
+    "description": "Os métodos mais assertivos para se trabalhar com o Tarot é por meio da quantidade de cartas! Use uma carta para reflexões e conselhos; Use duas cartas para resolução de problemas mais simples. Por fim use a jogada de três cartas, onde você pode definir cada uma como um tempo 'passado', 'presente' e 'futuro'.",
+    "photoCover":"https://conteudo.imguol.com.br/c/entretenimento/e6/2021/08/05/a-mensagem-de-cada-carta-do-taro-veja-o-que-os-arquetipos-podem-ensinar-1628189201743_v2_900x506.jpg.webp"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Veja o que algumas combinações de cartas significam!",
+    "description": "Apesar do tarot não ter cartas positivas e nem negativas, por meio de alguns arcanos conseguimos ter mais noção de qual lado estamos falando. Por exemplo, a carta do sol junto da estrela traz a promessa de muita felicidade, prosperidade e realização, por serem cartas positivas; Já a carta do diabo junto da carta da morte pode significar uma espécie de prisão relacionada a alguma situação da qual a pessoa ainda não encerrou de fato.",
+    "photoCover":"https://diariodonoroeste.com.br/wp-content/uploads/2022/05/11559_XL-1024x682-1.jpg"
+  },
+  {
+    "id":"3",
+    "title": "O significado de cada naipe do Tarot",
+    "description": "No tarot temos 4 naipes assim como no baralho normal, sendo eles: ouros que representa terra, copas que representa a água, paus que representa o fogo e espadas que representa o ar. Os naipes são diretamente ligados aos 4 elementos e suas caracteristicas. O naipe de ouros fala sobre nosso lado material e tudo aquilo que traz estabilidade na vida, já o naipe de copas fala sobre nossas emoções e principalmente sobre nossos relacionamentos e objetivos e conquistas emocionais, já o naipe de espadas fala sobre nosso mental e como nossos pensamentos influenciam em todos o script que acontece na nossa vida. Por último, o naipe de paus representa a força de vontade, a ação em si e como ela interfere na nossa realização.",
+    "photoCover":"https://www.astrocentro.com.br/blog/wp-content/uploads/2017/04/astrologia-astrocentro9.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Combinações positivas no Tarot",
+    "description": "As combinações positivas do tarot se dão por meio de sequências positivas de cartas, por exemplo a carta da estrela seguida da carta do sol seguido da carta do 10 de ouros. As cartas que em uma tiragem mais representa felicidade e coisas positivas são a os reis, rainhas, imperatriz e imperador, seguido dos arcanos maiores da estrela, do sol e do mundo, o naipe de ouros e de copas concentram as melhores e mais positivas cartas devido ao fato de envolver realização.",
+    "photoCover":"https://s2-techtudo.glbimg.com/s0wvZjZ6Ga8aQgOfeJAIlrVeW2o=/0x0:4868x2738/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/d/L/3lKeaFSy6Fz3CD5Q3HhQ/petr-sidorov-d3szbcaemhq-unsplash.jpg"
+  },
+  {
+    "id":"5",
+    "title": "Combinações negativas no Tarot",
+    "description": "As combinações negativas do tarot se dá por uma sequência negativa de cartas que fazem com que na maioria dos casos, a jogada sirva como um alerta ao consulente de que a situação da qual ele está envolvida, não é muito boa. Nelas podemos encaixar a maioria das cartas de espadas, assim como algumas cartas do naipe de paus e alguns arcanos maiores como a carta do eremita, do pendurado, da morte, do diabo e da torre. Que não necessariamente significa que a situação nunca vai mudar, mas sim de que pra mudança ocorrer você precisa sair da situação que se encontra nesse momento atual",
+    "photoCover":"https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2021/04/16/360818020-cartas-de-tarot-17342.jpg"
   }
 ]
